be29002b52dac1d7a67fb7aee96cdaf73e9cd2fb
FixbroYT <fixbro2011@gmail.com>
2025-08-09 17:09:12 +0200

    Fix

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 DB_URL = os.getenv("DATABASE_URL")
-print(DB_URL, os.getenv("DATABASE_URL"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 STRIPE_APIKEY = os.getenv("STRIPE_APIKEY")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
