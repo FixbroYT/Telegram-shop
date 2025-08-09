6f320a21559771360e09c1e0c9fb30bb3512bf67
FixbroYT <fixbro2011@gmail.com>
2025-08-09 16:57:20 +0200

    Fix

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 DB_URL = os.getenv("DATABASE_URL")
+print(DB_URL, os.getenv("DATABASE_URL"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 STRIPE_APIKEY = os.getenv("STRIPE_APIKEY")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
--- database/models.py
+++ database/models.py
@@ -8,7 +8,6 @@ from config import DB_URL
 
 logger = logging.getLogger(__name__)
 
-print(DB_URL)
 engine = create_async_engine(DB_URL)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
